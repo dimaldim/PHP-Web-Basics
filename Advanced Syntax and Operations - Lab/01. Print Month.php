e27037b28a20a52b2421e4aaebee4ace72295537
dimaldim <antraks92@gmail.com>
2019-05-14 20:49:13 +0300

    Upload task 1

---
+++ Advanced Syntax and Operations - Lab/01. Print Month.php
@@ -0,0 +1,11 @@
+<?php
+$months = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+$input = readline();
+
+if ($input >= 1 && $input <= 12) {
+    echo $months[$input - 1];
+} else {
+    echo "Invalid Month!";
+}
